65f8e0a6a20f1e3e331dc36c1820f024788c2192
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-04-19 09:38:31 +0200

    moved try block

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-})
+  name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-module.exports = mongoose.model('user', UserSchema) 
+module.exports = mongoose.model('user', UserSchema);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,9 +6,8 @@ const CryptoJS = require('crypto-js');
 // HÄMTA SPECIFIK USER
 router.post('/', async (req, res, next) => {
   const { id } = req.body;
-  const user = await UserModel.findById({ _id: id });
-
   try {
+    const user = await UserModel.findById({ _id: id });
     res.status(200).json(user);
   } catch (err) {
     console.error(err);
@@ -19,14 +18,20 @@ router.post('/', async (req, res, next) => {
 // SKAPA USER
 router.post('/add', async (req, res, next) => {
   console.log(req.body);
-  const user = new UserModel(req.body);
-  let encryptPassword = req.body.password;
-  encryptPassword = CryptoJS.AES.encrypt(user.password, 'salt key').toString();
-  user.password = encryptPassword;
-  await user.save();
 
   try {
-    res.status(201).json(user);
+    const user = new UserModel(req.body);
+    let encryptPassword = req.body.password;
+    encryptPassword = CryptoJS.AES.encrypt(
+      user.password,
+      'salt key'
+    ).toString();
+    user.password = encryptPassword;
+    await user.save();
+
+    const sendUser = { name: user.name, id: user._id };
+
+    res.status(201).json(sendUser);
   } catch (err) {
     console.error(err);
     res.status(500).json({ msg: err });
@@ -36,23 +41,25 @@ router.post('/add', async (req, res, next) => {
 // LOGGA IN USER
 router.post('/login', async (req, res, next) => {
   const { name, password } = req.body;
-  const user = await UserModel.findOne({ name });
-
-  if (!user) {
-    return res.status(400).json({ msg: 'No user found' });
-  }
-
-  const decryptPassword = CryptoJS.AES.decrypt(
-    user.password,
-    'salt key'
-  ).toString(CryptoJS.enc.Utf8);
-
-  if (decryptPassword !== password) {
-    return res.status(400).json({ msg: 'Incorrect credentials' });
-  }
-
   try {
-    res.status(200).json(user);
+    const user = await UserModel.findOne({ name });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'No user found' });
+    }
+
+    const decryptPassword = CryptoJS.AES.decrypt(
+      user.password,
+      'salt key'
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (decryptPassword !== password) {
+      return res.status(400).json({ msg: 'Incorrect credentials' });
+    }
+
+    const sendUser = { name: user.name, id: user._id };
+
+    res.status(200).json(sendUser);
   } catch (err) {
     console.error(err);
     res.status(500).json({ msg: err });
