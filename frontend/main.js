3419d530ec069f242002fdfbcfe5b1524efc8ee0
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-04-19 10:55:55 +0200

    ✨ added basic chat structure in html and scss

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,7 @@
 import './style.scss';
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3003');
 
 socket.on('chat', (arg) => {
   console.log('chat', arg);
