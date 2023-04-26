d44aed46cf9909da553e34cd7c470a6a3160d1d2
Grapensparr <liza.grapensparr@gmail.com>
2023-04-26 21:13:03 +0200

    Added timer functionality

--- backend/routes/referenceImage.js
+++ backend/routes/referenceImage.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const ReferenceImage = require('../models/referenceImage');
 
 router.get('/randomGameImage', async (req, res) => {
-    try {
-      const count = await ReferenceImage.countDocuments();
-      const randomIndex = Math.floor(Math.random() * count);
-      const referenceImage = await ReferenceImage.findOne().skip(randomIndex);
-      res.status(200).json(referenceImage);
-    } catch (err) {
-      console.error(err);
-    }
-  });
+  try {
+    const count = await ReferenceImage.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const referenceImage = await ReferenceImage.findOne().skip(randomIndex);
+    res.status(200).json(referenceImage);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 module.exports = router;
