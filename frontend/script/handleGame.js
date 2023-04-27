4ce567e925e34036d18cd310e3832892571c25bf
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-04-27 12:49:06 +0200

    added save and load buttons

--- frontend/script/handleGame.js
+++ frontend/script/handleGame.js
@@ -1,12 +1,16 @@
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
 const socket = io('http://localhost:3000');
+const user = JSON.parse(localStorage.getItem('user'));
+console.log(user);
 
 export function printGame() {
   const game = document.querySelector('#game');
-  game.innerHTML += /*html*/`
+  game.innerHTML += /*html*/ `
   <button id='joinButton'>Join game</button>
   <button id='saveReferenceButton'>Save reference image</button>
   <button id='resultButton'>Show results</button>
+  <button id='saveButton'>Save</button>
+  <button id='loadButton'>Load</button>
   <h2 id="timer"></h2>
   <table id="grid" border="1"></table>
   <canvas id='referenceCanvas' width='150' height='150'></canvas>`;
@@ -45,10 +49,10 @@ export function printGame() {
         fullGameMessage.remove();
       }
       const message = document.createElement('h2');
-      message.id = 'fullGameMessage'
+      message.id = 'fullGameMessage';
       message.innerText = 'The game is currently full';
       gridTable.before(message);
-    })
+    });
   });
 
   socket.on('reloadButtons', () => {
@@ -57,12 +61,12 @@ export function printGame() {
       endGameButton.remove();
     }
 
-    game.prepend(joinButton)
+    game.prepend(joinButton);
     joinButton.disabled = false;
   });
 
-  const saveButton = document.getElementById('saveReferenceButton');
-  saveButton.addEventListener('click', () => {
+  const saveRefButton = document.getElementById('saveReferenceButton');
+  saveRefButton.addEventListener('click', () => {
     const grid = [];
     for (let y = 0; y < 15; y++) {
       let row = [];
@@ -96,7 +100,7 @@ export function printGame() {
   resultsButton.addEventListener('click', () => {
     const grid = [];
     const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
+    cells.forEach((cell) => {
       grid.push(cell.style.backgroundColor);
     });
 
@@ -115,6 +119,11 @@ export function printGame() {
     } else {
       console.log('Reference image not found');
     }
+  });
+
+  const saveBtn = document.querySelector('#saveButton');
+  saveBtn.addEventListener('click', () => {
+    socket.emit('saveImage', { userId: user.id });
   });
 }
 
@@ -153,20 +162,25 @@ socket.on('removeMessage', () => {
 socket.on('clearCanvas', () => {
   const referenceCanvas = document.getElementById('referenceCanvas');
   const referenceContext = referenceCanvas.getContext('2d');
-  referenceContext.clearRect(0, 0, referenceCanvas.width, referenceCanvas.height);
-})
+  referenceContext.clearRect(
+    0,
+    0,
+    referenceCanvas.width,
+    referenceCanvas.height
+  );
+});
 
 socket.on('startTimer', () => {
-  const timer = document.getElementById('timer')
+  const timer = document.getElementById('timer');
   const minutesFromStart = 3;
   let time = minutesFromStart * 60;
   const myInterval = setInterval(() => {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
-    seconds = seconds < 10? "0" + seconds : seconds;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
     timer.innerHTML = `Time left: ${minutes}:${seconds}`;
-    time --;
+    time--;
     time = time < -1 ? -1 : time;
 
     if (time < 30) {
@@ -178,12 +192,12 @@ socket.on('startTimer', () => {
     if (time === -1) {
       clearInterval(myInterval);
       socket.emit('endGame');
-      timer.innerHTML = "";
+      timer.innerHTML = '';
     }
-  }, 1000)
+  }, 1000);
 
   socket.on('stopTimer', () => {
-      clearInterval(myInterval);
-      timer.innerHTML = "";
-  })
+    clearInterval(myInterval);
+    timer.innerHTML = '';
+  });
 });
